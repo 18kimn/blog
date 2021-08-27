7129ea8e7b04c04da1cef069919b7fdb236e4670
Nathan Kim <nathanckim18@gmail.com>
2021-08-27 14:09:31 -0400

    commenting out barely-started block of code

--- src/pages/projects/bio/index.js
+++ src/pages/projects/bio/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
+// import React from 'react'
 
-const bioInfo = [
-  {date, title, info, category},
-  {},
-  {},
-]
-const Bio = () => {
-  // bioInfo.map(info => )
-  return (
-    <>
-    </>
-  )
-}
+// const bioInfo = [
+//   {date, title, info, category},
+//   {},
+//   {},
+// ]
+// const Bio = () => {
+//   // bioInfo.map(info => )
+//   return (
+//     <>
+//     </>
+//   )
+// }
 
-export default Bio
+// export default Bio
