59fbb3a57e19b4d3fe1459ae9d136c7f7e4ebd9a
Nathan Kim <nathanckim18@gmail.com>
2021-08-27 14:22:01 -0400

    firebase+gatsby fix

--- src/utils/gatsby-node.esm.js
+++ src/utils/gatsby-node.esm.js
@@ -1,10 +1,8 @@
-import { generateSlug } from './stringUtils.js'
+import {generateSlug} from './stringUtils.js'
 import ESLintPlugin from 'eslint-webpack-plugin'
 import path from 'path'
 
-const onCreateWebpackConfig = ({
-  actions,
-}) => {
+const onCreateWebpackConfig = ({stage, actions, getConfig}) => {
   actions.setWebpackConfig({
     plugins: [
       new ESLintPlugin({
@@ -12,17 +10,30 @@ const onCreateWebpackConfig = ({
       }),
     ],
   })
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      externals: getConfig().externals.concat((context, request, callback) => {
+        const regex = /^@?firebase(\/(.+))?/
+        // exclude firebase products from being bundled
+        // so they will be loaded using require() at runtime.
+        if (regex.test(request)) {
+          return callback(null, 'umd ' + request)
+        }
+        callback()
+      }),
+    })
+  }
 }
 
-const createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
+const createPages = async ({actions, graphql, reporter}) => {
+  const {createPage} = actions
 
   const blogPostTemplate = path.resolve('src/templates/blog-post.js')
 
   const result = await graphql(`
     {
       allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
+        sort: {order: DESC, fields: [frontmatter___date]}
         limit: 1000
       ) {
         edges {
@@ -43,8 +54,7 @@ const createPages = async ({ actions, graphql, reporter }) => {
     return
   }
 
-
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  result.data.allMarkdownRemark.edges.forEach(({node}) => {
     const slug = node.frontmatter.slug
     const formattedSlug = slug ? slug : generateSlug(node.fileAbsolutePath)
 
@@ -56,5 +66,4 @@ const createPages = async ({ actions, graphql, reporter }) => {
   })
 }
 
-
-export { onCreateWebpackConfig, createPages }
+export {onCreateWebpackConfig, createPages}
