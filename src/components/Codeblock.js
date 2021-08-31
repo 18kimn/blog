5b7d141e7d836e3112458db9965ca18fa9663d0c
Nathan Kim <nathanckim18@gmail.com>
2021-08-31 16:15:38 -0400

    revisions to codeblock and leftist-quotes

--- src/components/Codeblock.js
+++ src/components/Codeblock.js
@@ -1,22 +1,62 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import {Paper} from '@material-ui/core'
 import PropTypes from 'prop-types'
+import * as d3 from 'd3'
+import useStyles from '../styles/PageStyles'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
+import ArrowRightIcon from '@material-ui/icons/ArrowRight'
 
-const Codeblock = ({msg, children}) => {
+const Codeblock = ({id, children}) => {
+  const classes = useStyles()
+  const [opened, setOpened] = useState(false)
+  const [msg, setMsg] = useState('show')
+
+  const showCode = useCallback(() => {
+    const fullHeight = d3
+      .select(`#${id} > .gatsby-highlight > pre > code`)
+      .node()
+      .getBoundingClientRect().height
+
+    const targetHeight = opened ? '0px' : `calc(${fullHeight}px + 2em)`
+    const targetPadding = opened ? '0em' : '1em'
+    const targetMsg = opened ? 'show' : 'hide'
+
+    d3.select(`#${id} > .gatsby-highlight > pre`)
+      .transition()
+      .duration(100)
+      .style('height', targetHeight)
+      .style('padding', targetPadding)
+      .on('end', () => {
+        setOpened((prevState) => !prevState)
+        setMsg(targetMsg)
+      })
+  }, [id, opened])
+
   return (
-    <details>
-      <summary style={{cursor: 'pointer'}}>
-        {msg ? msg : 'click to show code'}
-      </summary>
-      <Paper square={true} elevation={4}>
+    <div className="codeBlock">
+      <button
+        id={'button' + id}
+        onClick={showCode}
+        className={classes.codeButton}
+      >
+        {opened ? <ArrowDropDownIcon /> : <ArrowRightIcon />}
+        {`click to ${msg} code`}
+      </button>
+      <Paper
+        square={true}
+        elevation={4}
+        id={id}
+        className={classes.codeContainer}
+      >
         {children}
       </Paper>
-    </details>
+    </div>
   )
 }
 
 Codeblock.propTypes = {
   msg: PropTypes.string,
+  id: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
