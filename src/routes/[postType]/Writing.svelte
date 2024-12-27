ac3f06d7c28c989e8aeeba4e18b9cefb6b1267fe
nathan kim <nathanckim18@gmail.com>
2024-12-27 01:01:33 -0500

    writing

--- src/routes/[postType]/Writing.svelte
+++ src/routes/[postType]/Writing.svelte
@@ -1,5 +1,8 @@
 <h2>Writing</h2>
 <p>
-  Of all kinds. I'd love to hear of any feedback or
-  comments!
+  Misc., but mostly about random technical stuff I happen to
+  want to write up, because anything more serious (on race,
+  and empire, and politics, etc) are things I am trying to
+  stuff into an academic paper. See links on the home page
+  or in the CV page for more serious work.
 </p>
